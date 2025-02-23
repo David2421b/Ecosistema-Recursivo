65ce0eaf75e7b53553018ed6e57a3eda8ae82860
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-02-23 00:38:19 -0500

    Creacion de las clases

---
+++ Ecosistema.py
@@ -0,0 +1,16 @@
+class Depredadores:
+
+    def __init__(self):
+        pass
+
+
+class Presas:
+    
+    def __init__(self):
+        pass
+
+
+class Plantas:
+
+    def __init__(self):
+        pass

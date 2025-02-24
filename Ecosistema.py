370a269eead6aef5c2e5eb8f1b637453770367df
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-02-23 20:20:19 -0500

    creacion de obejtos y guardado en lista

--- Ecosistema.py
+++ Ecosistema.py
@@ -6,17 +6,28 @@ class Depredadores:
         self.nombre: int = nombre
         self.vida: int = vida
 
+    
+    def __repr__(self):
+        return f"{self.nombre} \n Vida: {self.vida}"
+
 class Presas:
     
     def __init__(self, nombre: str, vida: int):
+        self.nombre: str = nombre
         self.vida: int = vida
+    
+    def __repr__(self):
+        return f"{self.nombre} \n Vida: {self.vida}"
 
 
 class Plantas:
 
-    def __init__(self, nombre: str, vida = 0):
+    def __init__(self, nombre: str, vida: int = 0):
         self.nombre: str = nombre
+        self.vida: int = vida
 
+    def __repr__(self):
+        return f"{self.nombre}"
 
 
 def crear_matriz(n: int, i: int = 0, j: int = 0, fila: list[int] = [], matriz: list[list[int]] = []) -> list[list[int]]:
@@ -31,14 +42,9 @@ def crear_matriz(n: int, i: int = 0, j: int = 0, fila: list[int] = [], matriz: l
     return crear_matriz(n, i, j + 1, fila, matriz)
 
 
-def crear_obejtos(num_objects: int, idx = 0, lista: list[object] = []) -> list[object]:
+def crear_objetos(num_objects: int, idx = 0, lista: list = []) -> list[object]:
     if num_objects == idx:
         return lista
-    
-    ran1, ran2, ran3 = random.randint(0, len(name_depredadores)), random.randint(0, len(name_presas)), random.randint(0, len(naem_plantas_comestibles))
-
-    vida_depredadores, vida_presas = random.randint(1, 11), random.randint(1, 8)
-
     
     name_depredadores = [
         "León", "Tigre", "Lobo", "Águila", "Tiburón blanco",
@@ -60,12 +66,15 @@ def crear_obejtos(num_objects: int, idx = 0, lista: list[object] = []) -> list[o
         "Cebolla", "Perejil", "Pepino", "Uva", "Brócoli"
     ]
 
+    ran1, ran2, ran3 = random.randint(0, len(name_depredadores) - 1), random.randint(0, len(name_presas) - 1), random.randint(0, len(naem_plantas_comestibles) - 1)
+    vida_depredadores, vida_presas = random.randint(1, 11), random.randint(1, 8)
+
     lista.append(Depredadores(name_depredadores[ran1], vida_depredadores))
     lista.append(Presas(name_presas[ran2], vida_presas))
     lista.append(Plantas(naem_plantas_comestibles[ran3]))
 
 
-    return crear_obejtos(num_objects, idx + 1, lista)
+    return crear_objetos(num_objects, idx + 1, lista)
 
     
 
@@ -83,11 +92,7 @@ def asignar_elemento(matriz: list[list[int]], elemento: object):
     return matriz
 
 
-
-
-predador1 = Depredadores("Dinosaurio", 5, 10)
-
-print(asignar_elemento(crear_matriz(5), predador1.nombre))
+print(crear_objetos(5))
 
 
 
